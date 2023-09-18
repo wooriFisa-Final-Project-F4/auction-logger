7f3ed45a3244cf8539896f8b2f1415bbff29e9c9
Jiwoon <kfromh0136@Naver.com>
2023-09-18 09:53:54 +0900

    [fix] controller 수정
    @RequestParam -> @RequestHeader

--- src/main/java/f4/domain/controller/HistoryController.java
+++ src/main/java/f4/domain/controller/HistoryController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -23,8 +20,8 @@ public class HistoryController {
     return ResponseEntity.status(HttpStatus.OK).body(historyService.findAll());
   }
 
-  @GetMapping("/user/{id}")
-  public ResponseEntity<?> findById(@PathVariable Long userId){
+  @GetMapping("/user")
+  public ResponseEntity<?> findById(@RequestHeader("userId") Long userId){
     log.info("입찰 전체 기록 조회 : userId : {}",userId);
     return ResponseEntity.status(HttpStatus.OK).body(historyService.findByUserId(userId));
   }
